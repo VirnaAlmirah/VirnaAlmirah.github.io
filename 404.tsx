79cfe218720e53e5fb8411f9792a73c9e399931f
VirnaAlmirah <virnalmirah@gmail.com>
2024-10-22 15:35:40 +0700

    css

--- 404.tsx
+++
@@ -1,8 +0,0 @@
-export default function custom404() {
-    return (
-        <div>
-<h1 className="title-not-found">OOoops......</h1>
-<h1 className="title-not-found">This Page Does Not Exist Now</h1>
-        </div>
-    )
-}
